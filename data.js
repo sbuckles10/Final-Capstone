4bdf7246f149a2db3fbe6b0e4732727ec826513f
sbuckles10 <sbuckles88@gmail.com>
2021-12-09 23:08:10 -0800

    update

--- data.js
+++ data.js
@@ -1,75 +1,50 @@
 const data = [{
   "id": 1,
-  "name": "happy",
-  "image": "./resources/images/happy_spongebob.gif",
-  "desc": "Happiness lies in the joy of achievement and the thrill of creative effort.",
-  "price": 5.99
+  "name": "Poutine",
+  "image": "./images/poutine2.jpeg",
+  "desc": "Classic poutine- Fries, gravy, cheese curds",
+  "price": 10.99
 }, {
   "id": 2,
-  "name": 'sad',
-  "image": './resources/images/sad_spongebob.gif',
-  "desc": "Tears are words that can’t be explained.",
-  "price": 5.99
+  "name": 'Bacon Poutine',
+  "image": './images/bacon-poutine.jpeg',
+  "desc": "Bacon Poutine- Our classic poutine with hickory smnoked bacon crumbled on top",
+  "price": 12.99
 }, {
   "id": 3,
-  "name": 'angry',
-  "image": "./resources/images/angry_1.gif",
-  "desc": "Anger is like a storm rising up from the bottom of your consciousness. When you feel it coming, turn your focus to your breath.",
-  "price": 5.99
+  "name": 'Buffalo Chicken Poutine',
+  "image": "./images/Buffalo-Chicken.jpeg",
+  "desc": "Buffalo Chicken Poutine-Our classic poitine with chicken and buffalo sauce drizzled on top",
+  "price": 14.99
 }, {
   "id": 4,
-  "name": 'calm',
-  "image": "./resources/images/calm_woman.gif",
-  "desc": "To be calm is the highest achievement of the self.",
-  "price": 5.99
+  "name": 'Veggie Poutine',
+  "image": "./images/vegie-poutine.jpeg",
+  "desc": "Vegetarian Poutine- Our classic poutine made with vegetarian gravy",
+  "price": 10.99
 }, {
   "id": 5,
-  "name": 'curious',
-  "image": "./resources/images/curious_owl.gif",
-  "desc": "I have no special talents. I am only passionately curious.",
+  "name": 'Beavertail',
+  "image": "./images/beavertail.jpeg",
+  "desc": "Beavertail- A Canadian wintertime favourite, a deep fried desert covered in cinnamon sugar",
   "price": 5.99
 }, {
   "id": 6,
-  "name": 'disgust',
-  "image": "./resources/images/disgust_theoffice.gif",
-  "desc": "Not all disgust need produce symptoms of nausea, nor all nausea mark the presence of disgust.",
-  "price": 5.99
+  "name": 'Split Pea Soup',
+  "image": "./images/split-pea.jpeg",
+  "desc": "Split Pea Soup- A less known Canadian favourite, a soup made from dried split peas",
+  "price": 6.99
 }, {
   "id": 7,
-  "name": 'energetic',
-  "image": "./resources/images/energetic_puppy.gif",
-  "desc": "Be the energy you want to attract.",
-  "price": 5.99
+  "name": 'Hawaiian Pizza',
+  "image": "./images/Hawaiian.jpeg",
+  "desc": "Hawaiian Pizza- Does Pineapple belong on pizza? Unknowing to many, this conrtoversial pizza has its origins traced back to Canada making it a true Canadian classic",
+  "price": 12.99
 }, {
   "id": 8,
-  "name": 'gloomy',
-  "image": "./resources/images/gloomy_girl.gif",
-  "desc": "All was black, gloomy and awful. There was no light at the end of the tunnel - or if there was, it was an oncoming train.",
-  "price": 5.99
-}, {
-  "id": 9,
-  "name": 'scared',
-  "image": "./resources/images/scared_spongebob.gif",
-  "desc": "The only thing we have to fear is fear itself.",
-  "price": 5.99
-}, {
-  "id": 10,
-  "name": 'shy',
-  "image": "./resources/images/shy_dog.gif",
-  "desc": "When a shy person smiles, it's like the sun coming out.",
-  "price": 5.99
-}, {
-  "id": 11,
-  "name": 'sleepy',
-  "image": "./resources/images/sleepy_cat.gif",
-  "desc": "Your future depends on your dreams, so go to sleep.",
-  "price": 5.99
-}, {
-  "id": 12,
-  "name": 'surprised',
-  "image": "./resources/images/surprised_2.gif",
-  "desc": "The only thing that should surprise us is that there are still some things that can surprise us.",
+  "name": 'Caesar',
+  "image": "./images/caesar.jpeg",
+  "desc": "Caesar- A classic adult beverage in Canada.  Very similar to a Bloody Mary except made with Clamato juice instead of just tomato juice, which is a mix between tomato and clam juice",
   "price": 5.99
 }]
-
 export default data;
--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 const itemsContainer = document.getElementById('items')
 import data from './data.js'
+const itemList = document.getElementById('item-list')
+const cartQty = document.getElementById('cart-qty')
+const cartTotal= document.getElementById('cart-total')
+
+itemList.innerHTML = '<li> Hello World</li>'
 
 
 for (let i = 0; i < data.length; i += 1) {
-
     const newDiv = document.createElement('div');
     newDiv.className = 'item'
     const img = document.createElement('img');
@@ -11,6 +15,121 @@ for (let i = 0; i < data.length; i += 1) {
     img.width = 300
     img.height = 300
     newDiv.appendChild(img)
-    console.log(img)
     itemsContainer.appendChild(newDiv)
+    const desc = document.createElement('P')
+    desc.innerText = data[i].desc
+    newDiv.appendChild(desc)
+    itemsContainer.appendChild(newDiv)
+    const price = document.createElement('P')
+    price.innerText = data[i].price
+    newDiv.appendChild(price)
+    const button = document.createElement('button')
+    button.id = data[i].name
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to Cart"
+    newDiv.appendChild(button)
 }
+    const all_items_button = Array.from(document.querySelectorAll("button"))
+    all_items_button.forEach(elt => elt.addEventListener('click', () => {
+    addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
+    showItems()
+}))
+
+itemList.onclick = function(e) {
+//console.log("Clicked List!!")
+//console.log(e.target)
+if(e.target && e.target.classList.contains('remove')){
+
+  const name = e.target.dataset.name
+  removeItem(name)
+} else if (e.target && e.target.classList.contains('remove-one')){
+  const name = e.target.dataset.name
+  removeItem(name, 1)
+}else if (e.target && e.target.classList.contains('add-one')){
+  const name = e.target.dataset.name
+  addItem(name, 1)
+}
+showItems()
+}
+const cart = []
+
+function addItem(name, price) {
+  for (let i = 0; i < cart.length; i += 1){
+    if (cart[i].name === name) {
+      cart[i].qty += 1
+      return
+
+    }
+  }
+
+  const item = { name, price, qty: 1 }
+  cart.push(item)
+}
+
+function showItems() {
+    const qty = getQty()
+    cartQty.innerHTML = `You have ${qty} items in your cart`
+
+  let itemStr = ''
+  for (let i = 0; i < cart.length; i += 1){
+      // console.log(`- ${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+
+
+      const {name, price, qty} = cart[i]
+
+      itemStr += `<li>
+      ${name} $${price} x ${qty} = ${qty * price}
+      <button class="remove" data-name="${name}">Clear</button>
+      <button class="add-one" data-name="${name}"> + </button>
+      <button class="remove-one" data-name="${name}"> - </button>
+      </li>`
+  }
+  itemList.innerHTML = itemStr
+
+
+  cartTotal.innerHTML = `Total in cart: $${getTotal()}`
+}
+
+function getQty () {
+  let qty = 0
+  for (let i = 0; i < cart.length; i += 1) {
+    qty += cart[i].qty
+  }
+  return qty
+}
+
+function getTotal() {
+  let total = 0
+  for (let i = 0; i < cart.length; i += 1) {
+    total += cart[i].price * cart[i].qty
+  }
+  return total.toFixed(2)
+}
+
+function removeItem(name, qty = 0){
+  for (let i = 0; i < cart.length; i += 1) {
+    if (cart[i].name === name) {
+      if(qty > 0) {
+        cart [i].qty -= qty
+      }
+
+      if (cart[i].qty < 1 || qty === 0) {
+        cart.splice(i, 1)
+      }
+      showItems()
+
+      return
+    }
+  }
+}
+
+addItem('Poutine', 10.99)
+addItem('Bacon Poutine', 12.99)
+addItem('Poutine', 10.99)
+addItem('Poutine', 10.99)
+addItem('Poutine', 10.99)
+removeItem('Poutine', 1)
+
+
+
+showItems ()
